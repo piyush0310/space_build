543d1b8ead086bee1973c5bdcc576d330662e626
Dhruv Kaushik <dhruvkaushik150@gmail.com>
2026-08-22 12:11:54 +0530

    update code

--- src/components/MagazineSection.tsx
+++ src/components/MagazineSection.tsx
@@ -1,13 +1,9 @@
 "use client";
-
 import Link from "next/link";
-
 export default function MagazineSection() {
   const pdfUrl = "/magazine/space build.pdf";
-
   return (
     <section className="w-full bg-[#FFF2EB]">
-
     </section>
   );
 }
