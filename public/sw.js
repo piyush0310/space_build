3f65d0b3f560acb5bc1ec2a861dc80094eea5357
Dhruv Kaushik <dhruvkaushik150@gmail.com>
2026-09-07 21:48:19 +0530

    remove

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-// Retire legacy service-worker registrations for this site.
-// This app does not use offline caching or intercept network requests.
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.registration.unregister());
-});
